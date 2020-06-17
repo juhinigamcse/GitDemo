baa0a8bb112d4f01ea2da192d7c28e4e567bff09
juhi <juhinigamcse@gmail.com>
2020-06-16 21:43:59 -0500

    Juhi First Commit

---
+++ src/test/java/qaclickacademy/Mavenjava/AppiumTest.java
@@ -0,0 +1,16 @@
+package qaclickacademy.Mavenjava;
+
+import org.testng.annotations.Test;
+
+public class AppiumTest {
+
+	@Test
+	public void NativeAppAndroid () {
+		System.out.println("NativeAppAndroid");
+	}
+	
+	@Test
+	public void deleteTwitter () {
+		System.out.println("deleteTwitter");
+	}
+}
